f43b341d8950fd7d258f8fb87591a6eb17463358
Milena Fleming <milenasapucaia29@gmail.com>
2022-12-07 11:31:23 +0000

    feat: initializing database configuration

---
+++ src/backend/config.ts
@@ -0,0 +1,14 @@
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  }
+   
+  // Inicializando firebase
+  const app = !getApps().length ? initializeApp( firebaseConfig ) : getApp()
+  const dataBase = getFirestore(app);
+   
+  export { dataBase }
